826b0ea3afec357063b38a47444a4b52dd94b2f4
yaoyizhi <email@yaoadou888>
2017-03-22 10:35:36 +0800

    have group model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,21 @@
 class GroupsController < ApplicationController
+    def index
+        @groups = Group.all
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = Group.find(params[:id])
+        @group.new(group_params)
+        redirect_to groups_path
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:title, :description)
+    end
 end
