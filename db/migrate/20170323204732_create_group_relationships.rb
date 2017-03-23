adfd3aea424c4a89805cbfa91dc9021f065b384b
yaoyizhi <email@yaoadou888>
2017-03-24 04:58:29 +0800

    building group_relations to user and group

---
+++ db/migrate/20170323204732_create_group_relationships.rb
@@ -0,0 +1,8 @@
+class CreateGroupRelationships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_relationships do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323013540) do
+ActiveRecord::Schema.define(version: 20170323204732) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
