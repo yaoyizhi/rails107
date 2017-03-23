d14d27bd922d137d135fa5cf0b8849acc1bebb90
yaoyizhi <email@yaoadou888>
2017-03-24 05:26:47 +0800

    implement group relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323204732) do
+ActiveRecord::Schema.define(version: 20170323211549) do
 
   create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
