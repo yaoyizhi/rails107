f3a97f144cddb2f294f51fdd1a3620dcf24c6e61
yaoyizhi <email@yaoadou888>
2017-03-25 05:53:40 +0800

    implement edit&delete to account/posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,31 @@ class PostsController < ApplicationController
       end
   end
 
+    def edit
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+      end
+
+    def update
+        @group = Group.find(params[:group_id])
+        @post = Post.new(post_params)
+        @post.group = @group
+        @post.user = current_user
+        if @post.save
+            redirect_to group_path(@group)
+            flash[:notice] = 'Update Success'
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to group_path(@group)
+    end
+
     private
 
     def post_params
